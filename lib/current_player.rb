d7da9b2d33cd706d0966ff45744d790bf8753302
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-06-01 00:09:40 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,12 @@ def turn_count(board)
   counter
 end
 
-def current_player(board)
-  if turn_count(board).even?
-    "X"
-  else
-    "O"
-  end
-end
+current_player(board) = turn_count(board).even? ? "X" : "O"
+
+#def current_player(board)
+  #if turn_count(board).even?
+    #"X"
+  #else
+    #"O"
+  #end
+#end
